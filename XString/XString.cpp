a1e1a96d05b7f755f4f6c04f71e878da8ea3797d
Xudehua <dehua.xu@nbbsw.com>
2017-07-12 18:38:57 +0800

    V0.2

--- XString/XString.cpp
+++ XString/XString.cpp
@@ -1,7 +1,10 @@
 #include "XString.h"
 
 
-
+ XString::~XString()
+ {
+	delete[] data;
+ }
 
 XString::XString(void)
 {
@@ -10,7 +13,9 @@ XString::XString(void)
 
 XString::XString(const char *str)
 {
-	
+	int len = strlen(str);
+	data = new char[len + 1];
+	strcpy(data, str);
 }
 
 XString::XString(int number)
@@ -23,6 +28,28 @@ XString::XString(char c, int len)
 	
 }
 
+char* XString::strcpy(char *target, const char *source)
+{
+	while ((*target = *source) != '\0')
+	{
+		target ++;
+		source ++;
+	}
+
+	return target;
+}
+
+int XString::strlen(const char *target)
+{
+	int len = 0;
+	
+	while (*target != '\0')
+	{
+		len ++;
+	}
+
+	return len;
+}
 
 
 
--- XString/XString.h
+++ XString/XString.h
@@ -10,8 +10,17 @@ public:
 	XString(const char *str);
 	XString(int number);
 	XString(char c, int len);
+	
+	~XString();
 
 	static char* strcpy(char *target, const char *source);
+	static int strlen(const char *target);
+	static int numberlen(int number, int base = 10);
+
+	static QString fromInt(int number, int base = 10);
+	static QString fromDouble(double value);
+	static QString fromFloat(float value);
+
 
 private:
 	char *data;
