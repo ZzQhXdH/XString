c0855168496b79b2cea55925e83ea37ee2f14a45
Xudehua <dehua.xu@nbbsw.com>
2017-07-12 18:57:57 +0800

    V0.4

--- XString/XString.cpp
+++ XString/XString.cpp
@@ -1,6 +1,15 @@
 #include "XString.h"
 
 
+char XString::HexNumberList[] = 
+{
+	'0', '1', '2', '3',
+	'4', '5', '6', '7',
+	'8', '9', 'A', 'B',
+	'C', 'D', 'E', 'F',
+};
+
+
  XString::~XString()
  {
 	delete[] data;
@@ -8,7 +17,7 @@
 
 XString::XString(void)
 {
-	
+
 }
 
 XString::XString(const char *str)
@@ -18,14 +27,19 @@ XString::XString(const char *str)
 	strcpy(data, str);
 }
 
-XString::XString(int number)
+XString::XString(int number, int base)
 {
-	
+	int len = numberlen(number);
+
+	data = new char[len + 1];
+	fromInt(data, number, base);
 }
 
 XString::XString(char c, int len)
 {
-	
+	data = new char[len + 1];
+	charset(data, c, len);
+
 }
 
 char* XString::strcpy(char *target, const char *source)
@@ -51,13 +65,41 @@ int XString::strlen(const char *target)
 	return len;
 }
 
+int XString::numberlen(int number, int base)
+{
+	int len = 1;
 
+	while (number /= base)
+	{
+		len ++;
+	}
 
+	return len;
+}
 
+void XString::fromInt(char *target, int number, int base)
+{
+	for (int i = 0; i < len; i ++)
+	{
+		target[len - i - 1] = HexNumberList[number % base];
+		number /= base;
+	}
+	target[len] = '\0';
+}
 
+void XString::charset(char *s, char c, int len)
+{
+	while (len --)
+	{
+		*s = c;
+		s ++;
+	}
+}
 
-
-
+XString* XString::fromInt(int number, int base)
+{
+	return new XString(number, base);
+}
 
 
 
--- XString/XString.h
+++ XString/XString.h
@@ -8,22 +8,27 @@ class XString
 public:
 	XString(void);
 	XString(const char *str);
-	XString(int number);
+	XString(int number, int base = 10);
 	XString(char c, int len);
 	
 	~XString();
+	
+	int toInt(void);
+	double toDouble(void);
 
 	static char* strcpy(char *target, const char *source);
 	static int strlen(const char *target);
 	static int numberlen(int number, int base = 10);
+	static void charset(const char *s, char c, int len);
+	static void fromInt(char *target, int number, int base = 10);
 
-	static QString fromInt(int number, int base = 10);
-	static QString fromDouble(double value);
-	static QString fromFloat(float value);
-
-
+	static XString* fromInt(int number, int base = 10);
+	static XString* fromDouble(double value);
+	static XString* fromFloat(float value);
+	
 private:
 	char *data;
-
+	
+	static const char HexNumberList[];
 };
 
